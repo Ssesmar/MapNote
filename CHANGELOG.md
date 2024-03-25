2d6d043bd91beef12565cfed9d9b79503eb90966
BadBoyBarny <119746693+Ssesmar@users.noreply.github.com>
2024-03-25 08:13:19 +0100

    Add files via upload

---
+++ CHANGELOG.md
@@ -0,0 +1,38 @@
+# MapNote
+## [1.8.0b](https://github.com/Ssesmar/MapNote) (2024-03-21)
+- Note:
+
+-- add worldmapbutton for MapNotes
+
+
+- Description:
+REQUIRES --> HandyNotes <-- to be installed                                                                                    
+
+
+    A collection of various icons on various maps from previous expansions up to Dragonflight. Displays raid and dungeon entrances, portals, ships, zeppelins, exits and passage icons
+    Displays Horde and Alliance icons that can also be activated by the other faction
+    All symbols are clickable (except on the minimap) and have a function
+
+Map function with or without Shift. Simply select the “Shift Function” in the MapNotes menu on or off:
+
+    Shift+Right-click or Right-click adds a TomTom waypoint (except on the Azeroth map)
+    Shift+Left-click or Left-click on a Instance icon opens the corresponding dungeon in the adventure guide (only works if the map is NOT maximized)
+    Shift+Left-click or Left-click on a Portal, Exit, Passage, Ship, Zeppelin, Transport or Old Instance icon displays the associated map
+    For example: When you hover over a portal icon, the name of where that portal would go will appear. For example => Orgrimmar. If you click on this symbol, the map of Orgrimmar will open (Regardless of whether the map is reduced in size or in full screen, both work)
+
+MinimapButton:
+
+    Left-click opens or close the MapNote setting menu
+    Right-click opens or close the HandyNote setting menu
+    Shift+Right-click  hide the minimap button
+    Settings can also be found under: ESC -> Interface -> Addons -> HandyNotes -> Plugins -> MapNot
+
+Chat commands:
+
+    to show MapNotes info/help in chat: /mn, /MN
+    to open MapNotes menu: /mno, /MNO
+    to close MapNotes menu: /mnc, /MNC
+    to show minimapbutton: /mnb or /MNB
+    to hide minimapbutton: /mnbh or /MNBH
+
+Full supported languages: enUS, deDE, esMX, ptBR, itIT, zhCN, zhTW, ruRU ,koKR, frFR, esES
---
+++ Nodes/BlizzAreaPoisNodesInfo.lua
@@ -0,0 +1,58 @@
+local ADDON_NAME, ns = ...
+
+ns.BlizzAreaPoisInfo = { 
+    -- AllianceInfo
+    5846, -- Vol'dun
+    5847, -- Nazmir
+    5848, -- Zuldazar
+    5873, -- Dustwallow Marsh, Boat to Menethil Harbor, Wetlands
+    5874, -- Wetlands, Boat to Theramore Isle, Dustwallow Marsh
+    5875, -- Wetlands, Boat to Daggercap Bay, Howling Fjord
+    5876, -- Howling Fjord, Boat to Menethil Harbor, Wetlands
+    5877, -- Borean Tundra, Boat to Stormwind City
+    5878, -- Stormwind, Boat to Valiance Keep, Borean Tundra
+    5879, -- Stormwind, Boat to Boralus Harbor, Tiragarde Sound
+    5880, -- Tiragarde Sound, Boat to Stormwind City
+    5892, -- The Jade Forest, Portal to Stormwind City
+    6014, -- Stormwind Portal Room
+    7340, -- Thaldraszus, Boat to Stormwind
+    7335, -- Stormwind, Boat to Dragon Isle
+
+    -- Horde
+    5843, -- Drustvar
+    5844, -- Tiragarde Sound
+    5845, -- Stormsong Valley
+    5883, -- Northern Stranglethorn, Zeppelin to Orgrimmar
+    5884, -- Orgrimmar, Zeppelin to Grom'gol, Schlingendorntal
+    5885, -- Orgrimmar, Zeppelin to Warsong Hold, Borean Tundra
+    5886, -- Borean Tundra, Zeppelin to Orgrimmar
+    5887, -- Echo Isles, Boat to Dazar'alor, Zuldazar
+    5888, -- Zuldazar, Boat to Echo Isles, Durotar
+    5890, -- The Jade Forest, Portal to Orgrimmar
+    6015, -- Orgrimmar Portal Room
+    6138, -- Mechagon
+    7339, -- Thaldraszus, Zeppelin to Orgrimmar
+    7341, -- Durotar, Zeppelin to the Waking Shores, Dragon Isles
+
+    -- Neutral
+    5881, -- The Cape of Stranglethorn, Boat to Ratschet
+    5882, -- Northern Barrens, Boat to Booty Bay
+
+    -- City Alliance
+    8, -- Ironforge Icon
+    11, -- Wetlands Menethil Harbor
+    16, -- Stormwind City Icon
+    38, -- Darnassus
+    1785, -- Exodar Icon
+    5793, -- Rest of Darnassus
+
+    -- City Horde
+    29, -- Thunder Bluff Icon
+    33, -- Orgrimmar Horde Icon
+    41, -- Grom'gol Stranglethorn
+    1704, -- Silvermoon City Icon
+
+    -- City Neutral
+    1961, -- Shattrath
+    5056, -- The Dark Portal
+}
